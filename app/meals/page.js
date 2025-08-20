d187b4d37ffe780f759cb48c4b85bb5c79e6b1ce
Nigel Marshal <nigelmarshal@gmail.com>
2025-08-20 14:43:37 +0100

    (feat): Add meals grid and meal item components
    Introduced MealsGrid and MealItem components with associated CSS modules for displaying meals. Updated the meals page to use these components and improved its layout and styling. Also updated jsconfig.json for better module resolution and added TypeScript as a dev dependency.

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,22 @@
+import MealsGrid from "components/meals/meals.grid";
+import classes from "./page.module.css";
+import Link from "next/link";
+
 export default function Meals() {
   return (
-    <main>
-      <h1>Meals!</h1>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Tasty meals, created <span classes={classes.highlight}>by you</span>
+        </h1>
+        <p>Choose your favorite meal and enjoy it!</p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your favourite recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
   );
 }
