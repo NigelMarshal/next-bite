d52922c0dc6f12f83eeceaa6cf0b379365667fe5
Nigel Marshal <nigelmarshal@gmail.com>
2025-08-20 16:53:05 +0100

    (feat): Add meal details page with styling and data fetch
    Implemented a detailed meal view at [mealSlug] with styled layout and dynamic data fetching using getMealBySlug. Added page.module.css for custom styles and updated lib/meals.js to provide a function for fetching a meal by slug.

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,8 +1,32 @@
+import Image from "next/image";
+import classes from "./page.module.css";
+import { getMealBySlug } from "@/lib/meals";
+
 export default function MealDetails({ params }) {
+  const meal = getMealBySlug(params.mealSlug);
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
   return (
-    <main>
-      {params.mealSlug}
-      <h1>Meal Details!</h1>
-    </main>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} fill alt={meal.title} />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </main>
+    </>
   );
 }
